5132f97c1528991dda8ba67c427f389c34844827
hiroakihonda19910302 <hiroaki.honda3918@gmail.com>
2020-10-13 01:54:19 +0900

    review,favoraite機能追加

--- app/controllers/publics/favorites_controller.rb
+++ app/controllers/publics/favorites_controller.rb
@@ -1,10 +1,23 @@
 class Publics::FavoritesController < ApplicationController
 
 	def create
-		
+		@post = Post.find(params[:post_id])
+    favorite = @post.favorites.new(user_id: current_user.id)
+    if favorite.save
+    	redirect_to request.referer
+    else
+    	redirect_to request.referer
+    end
 	end
 
 	def destroy
-		
+		@post = Post.find(params[:post_id])
+    favorite = @post.favorites.find_by(user_id: current_user.id)
+    if favorite.present?
+    	favorite.destroy
+    	redirect_to request.referer
+    else
+    	redirect_to request.referer
+    end
 	end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -9,4 +9,12 @@ class Post < ApplicationRecord
 
 	validates :title, presence: true, length: {maximum:30,minimum:2}
 	validates :body, presence: true, length: {maximum:250,minimum:2}
+
+	def favorited_by?(user)
+		favorites.where(user_id: user).exists?
+	end
+
+	def gooded_by?(user)
+		goods.where(user_id: user).exists?
+	end
 end
